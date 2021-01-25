f5c1e8cea9f18eb94e5929d586b73c6084d9e670
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 16:28:18 +0530

    Add Passenger model to its own file for better organization

--- Commons/Passenger.swift
+++ Commons/Passenger.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+class Passenger: Codable, ObservableObject, Identifiable, CustomStringConvertible {
+    
+    internal init(id: String = UUID().uuidString) {
+        self.id = id
+    }
+    
+    convenience init(salutation: Salutation, firstName: String, middleName: String? = nil, lastName: String?) {
+        self.init()
+        self.salutation = salutation
+        self.firstName = firstName
+        self.middleName = middleName
+        self.lastName = lastName
+    }
+    
+    var id: String = UUID().uuidString
+    @Published var salutation: Salutation = .Mr
+    
+    @Published var firstName: String = ""
+    @Published var middleName: String?
+    @Published var lastName: String?
+    
+    var fullName: String {
+        return "\(salutation). \(firstName) \(middleName ?? .init()) \(lastName ?? .init())".trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
