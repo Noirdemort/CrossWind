db641c61efcd43c38eda7b53dbfc8ed42bd10183
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 09:25:02 +0530

    Add TelCom for radio contact

---
+++ Commons/TelCom.swift
@@ -0,0 +1,13 @@
+//
+//  TelCom.swift
+//  CrossWind
+//
+//  Created by Noirdemort on 22/01/21.
+//
+
+import Foundation
+
+struct TelCom: Codable {
+    let countryCode: Int
+    let number: Int
+}
