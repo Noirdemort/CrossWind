51f257ff639d9e6b342c43ee23b9a626c12495a9
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 16:41:02 +0530

    Add UI for showing phone number and country code... embedded inside BookingView

--- Shared/PhoneView.swift
+++ Shared/PhoneView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct PhoneView: View {
     @Binding var phone: TelCom
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer(minLength: 5)
+            TextField("Country Code", text: $phone.countryCode)
+                .keyboardType(.numberPad)
+            Spacer()
+            TextField("Phone Number", text: $phone.number)
+                .keyboardType(.numberPad)
+            Spacer(minLength: 5)
+        }
     }
 }
 
 struct PhoneView_Previews: PreviewProvider {
+    @State static var phone = TelCom(countryCode: .init(), number: .init())
+    
     static var previews: some View {
-        PhoneView()
+        PhoneView(phone: PhoneView_Previews.$phone)
     }
 }
