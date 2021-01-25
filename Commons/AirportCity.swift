4365046e8906768dae0004abe6b33f10382055f4
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 09:22:22 +0530

    Add airport city model for airport location representation

---
+++ Commons/AirportCity.swift
@@ -0,0 +1,14 @@
+//
+//  AirportCity.swift
+//  CrossWind
+//
+//  Created by Noirdemort on 22/01/21.
+//
+
+import Foundation
+
+
+struct AirportCity: Codable {
+    let cityCode: String
+    let cityName: String
+}
