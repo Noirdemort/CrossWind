7086a0b2b35ff655b0514a21a47d117c477c090e
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 09:20:47 +0530

    Add check-in target package

---
+++ CheckInClip/CheckInClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckInClipApp.swift
+//  CheckInClip
+//
+//  Created by Noirdemort on 22/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckInClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
