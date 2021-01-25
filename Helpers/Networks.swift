90389637ebcefb96288d2276c47130609165fffe
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 09:26:17 +0530

    Add network for generic network stuff

---
+++ Helpers/Networks.swift
@@ -0,0 +1,16 @@
+//
+//  Networks.swift
+//  CrossWind
+//
+//  Created by Noirdemort on 22/01/21.
+//
+
+import Foundation
+
+func auth(email: String, password: String) {
+    
+}
+
+func deploy<T: Codable>(_ : T, urlRequest: URLRequest) {
+    
+}
