ddfd1344f76cba54e45e69ae019854dc60bf91ab
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 16:44:04 +0530

    Add binding for passengers to receive and edit

--- Shared/PassengerView.swift
+++ Shared/PassengerView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct PassengerView: View {
+    @Binding var passengers: [Passenger]
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
