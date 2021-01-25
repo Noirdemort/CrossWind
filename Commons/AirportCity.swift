81e14fa017e1bb92e6c4eeb5351e8f2605c4a1b2
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 16:27:12 +0530

    Add CustomStringConvertible for representation and testing purposes

--- Commons/AirportCity.swift
+++ Commons/AirportCity.swift
@@ -8,11 +8,21 @@
 import Foundation
 
 
-struct AirportCity: Codable {
+struct AirportCity: Codable, CustomStringConvertible {
     let cityCode: String
     let cityName: String
+    
     enum CodingKeys: CodingKey {
         case cityCode
         case cityName
+    }
+    
+    var description: String {
+        return """
+            AirPort City
+
+            city code: \(cityCode)
+            city name: \(cityName)
+            """
     }
 }
