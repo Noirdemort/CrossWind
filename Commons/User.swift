7e096f6130bcbaa6095fb9f3aec5fdd729818dda
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 09:23:00 +0530

    Add User details model

---
+++ Commons/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  CrossWind
+//
+//  Created by Noirdemort on 22/01/21.
+//
+
+import Foundation
+
+
+enum Membership: String, RawRepresentable {
+    case gold
+    case silver
+    case platinum
+}
+
+struct User: Codable, Identifiable {
+    
+    var id: String = UUID().uuidString
+    
+    let name: String
+    let email: String
+    let secureHash: String
+    let membership: Membership.RawValue
+    let phone: TelCom?
+}
