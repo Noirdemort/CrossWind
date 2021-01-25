c805f2ce325aa270a779b9da3181fd53a6b38b94
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 16:44:38 +0530

    Add code for adding more passengers

--- Shared/PassengerView.swift
+++ Shared/PassengerView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct PassengerView: View {
     @Binding var passengers: [Passenger]
+    @StateObject private var passenger = Passenger()
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Form {
+                
+            Section(header: Text("Add Passenger")){
+            
+                Picker("Salutation", selection: $passenger.salutation){
+                    ForEach(Salutation.allCases){
+                        Text($0.rawValue).tag($0)
+                    }
+                }
+                
+                TextField("First Name", text: $passenger.firstName)
+                TextField("Middle Name (Optional)", text: $passenger.middleName.bound)
+                TextField("Last Name (Optional)", text: $passenger.lastName.bound)
+                Button("Add Passenger") {
+                    print(#function)
+                    if !passenger.firstName.isEmpty {
+                        print("adding passenger")
+                        passengers.append(passenger)
+                    } else {
+                        NSLog("Passenger details empty")
+                        print(passenger)
+                    }
+                }
+            }
     }
 }
 
