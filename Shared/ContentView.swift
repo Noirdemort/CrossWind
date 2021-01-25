1f0218d796cbeb1223af22a394f76e5bdb9d83ff
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 09:29:35 +0530

    Rename ContentView to BookingView to avoid confusion

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct BookingView: View {
     
 //    @StateObject var ticket = Ticket()
     
@@ -29,9 +29,9 @@ struct ContentView: View {
     
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct BookingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        BookingView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
 
--- Shared/CrossWindApp.swift
+++ Shared/CrossWindApp.swift
@@ -13,7 +13,7 @@ struct CrossWindApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BookingView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
