9e15b40c2bdc30443649968ac149130e2f3e3def
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 16:31:22 +0530

    Enhanced TelCom with Codable and CustomStringConvertible for easy translation and testing

--- Commons/TelCom.swift
+++ Commons/TelCom.swift
@@ -6,8 +6,74 @@
 //
 
 import Foundation
+import Combine
 
-struct TelCom: Codable {
-    let countryCode: Int
-    let number: Int
+//class TelCom: Codable, ObservableObject {
+//
+//    @Published var countryCode: String
+//    @Published var number: String
+//
+//    var isValidPublisher: AnyPublisher<Bool, Never> {
+//        Publishers.CombineLatest($countryCode, $number)
+//            .map { countryCode, number in
+//                return number.count == 10 && !countryCode.isEmpty
+//            }
+//            .eraseToAnyPublisher()
+//    }
+//
+//    var isValid: Bool {
+//        return !countryCode.isEmpty && number.count == 10
+//    }
+//
+//    enum CodingKeys: CodingKey {
+//        case countryCode
+//        case number
+//    }
+//
+//
+//    required init(from decoder: Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        countryCode = try container.decode(String.self, forKey: .countryCode)
+//        number = try container.decode(String.self, forKey: .number)
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//
+//        try container.encode(countryCode, forKey: .countryCode)
+//        try container.encode(number, forKey: .number)
+//    }
+//
+//}
+
+struct TelCom: Codable, CustomStringConvertible, CustomDebugStringConvertible {
+    
+    var debugDescription: String {
+        return description
+    }
+    
+
+    var description: String {
+        return """
+            TelCom Model
+            
+            country code: \(countryCode)
+            phone number: \(number)
+
+            Is this number valid? \(isValid)
+            """
+    }
+    
+    var countryCode: String
+    var number: String
+
+
+    var isValid: Bool {
+        return !countryCode.isEmpty && number.count == 10
+    }
+    
+    enum CodingKeys: CodingKey {
+        case countryCode
+        case number
+    }
 }
