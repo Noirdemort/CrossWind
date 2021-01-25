a4031c3791d7b83e464868598a9ca23dae5c4294
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-25 16:42:36 +0530

    Make string identifiable for simple representation via array of strings

--- Shared/ExtrasView.swift
+++ Shared/ExtrasView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+extension String: Identifiable {
+    
+    public var id: String { self }
+    
+}
+
 struct ExtrasView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
