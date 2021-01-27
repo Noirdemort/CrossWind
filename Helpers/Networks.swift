ed22f1a283e87c73edfffd0062677ab661af9a8c
Aryaman Gupta (Bravo 6) <aryaman.gupta@amadeus.com>
2021-01-27 12:43:08 +0530

    move and add some samples

--- Helpers/Networks.swift
+++ Helpers/Networks.swift
@@ -11,6 +11,34 @@ func auth(email: String, password: String) {
     
 }
 
-func deploy<T: Codable>(_ : T, urlRequest: URLRequest) {
+/// Makes the give url request, using http method defined in URLRequest. Tries to cast result to type T provided.
+/// - Parameter urlRequest: URLRequest with all variables
+/// - Returns: Codable conforming object as specified in the description
+/// - Throws: Throws Error if not able to cast to T or request
+func deploy<T: Codable>(urlRequest: URLRequest, completionHandler: @escaping (T) ->() ) throws {
     
+    let session = URLSession.shared
+    
+    session.dataTask(with: urlRequest) { data, response, error in
+        
+    }.resume()
+        
+}
+
+
+func giveTicket() -> Ticket {
+    let ticks = Ticket()
+    ticks.passengers = [Passenger(salutation: .Mr, firstName: "John", lastName: "Appleseed"), Passenger(salutation: .Mr, firstName: "Tim", lastName: "Cook")]
+    ticks.email = "email@s1guza.net"
+    ticks.journeys = [Journey(flyingClass: .firstClass,
+                              flightNumber: "Some Number",
+                              terminal: "String",
+                              travelDate: Date.init(),
+                              checkInTime: Date(),
+                              departure: AirportCity(cityCode: "BLR", cityName: "Bangalore"),
+                              departureTime: Date(),
+                              destination: AirportCity(cityCode: "KNP", cityName: "Kanpur"),
+                              arrivalTime: Date(), relayVia: nil)]
+    
+    return ticks
 }
